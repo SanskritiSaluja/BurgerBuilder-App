b3d380345f23204cfa364696382f7b36ef6810ac
SanskritiSaluja <sanskriti.saluja@tothenew.com>
2020-05-07 12:42:04 +0530

    Adding Ingredients Dynamically

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Burger.css'
+import BurgerIngredient from './BurgerIngredient/Burgeringredient'
+
+
+const burger = (props) => {
+   const transformedIngredients = Object.keys(props.ingredients)
+   .map(igKey => { 
+       return[...Array(props.ingredients[igKey])].map ((_,i)=> {
+           return <BurgerIngredient key= {igKey +i} type= {igKey}></BurgerIngredient>
+       })
+   })
+
+
+    return (
+           <div className= "Burger">
+            <BurgerIngredient type = "bread-top" />
+             {transformedIngredients}
+            <BurgerIngredient type = "bread-bottom" />
+           </div>
+    );
+}
+
+export default burger;
--- src/components/Burger/BurgerIngredient/Burgeringredient.js
+++ src/components/Burger/BurgerIngredient/Burgeringredient.js
@@ -1,10 +1,12 @@
-import React from 'react';
 
-import './BurgerIngrediet.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import './Burgeringredient.css';
 
-const burgerIngredient = (props) => {
+class BurgerIngredient extends Component {
+    render() {
     let ingredient = null;
-    switch(props.type) {
+    switch(this.props.type) {
         case ('bread-bottom') : 
         ingredient = <div className="BreadBottom"></div>
         break;
@@ -32,6 +34,11 @@ const burgerIngredient = (props) => {
     }
     return ingredient;
 }
+}
+
+BurgerIngredient.propTypes = {
+    type : PropTypes.string.isRequired
+}
 
 
-export default burgerIngredient;
+export default BurgerIngredient;
