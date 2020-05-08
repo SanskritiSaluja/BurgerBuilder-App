70056ff90c7258ab26af6b3fb5816bfa453ce140
SanskritiSaluja <sanskriti.saluja@tothenew.com>
2020-05-09 00:46:08 +0530

    Added Navigation(ToolBar and SideBar)

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import './SideDrawer.css';
+import Backdrop from '../../UI/BackDrop/BackDrop';
+import Aux from '../../../hoc/Aux';
+
+const sideDrawer = ( props ) => {
+    let attachedClasses = ["SideDrawer", "Close"];
+    if (props.open) {
+        attachedClasses = ["SideDrawer", "Open"];
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked = {props.closed}/>
+         <div className = {attachedClasses.join (' ')}>
+               <Logo height = "11%" />
+                 <nav>
+                    <NavigationItems />
+                </nav>
+        </div>   
+        </Aux>
+        );
+};
+
+export default sideDrawer;
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import './Toolbar.css'
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
+const toolbar = ( props ) =>{ 
+
+    return(
+    <header className= "Toolbar">
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
+        <Logo height = "80%"/>
+        <nav className = "DesktopOnly">
+            <NavigationItems />
+        </nav> 
+        
+    </header>
+);}
+
+export default toolbar;
